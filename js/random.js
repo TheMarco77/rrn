8bd13c1dfa2b1aa2ca57cf42f5fe21714f7bc3ee
Marco F. Ramos <mfilipe@gmail.com>
2020-05-02 17:23:08 -0400

    commit(!)

--- js/random.js
+++ js/random.js
@@ -1,4 +1,4 @@
 const dieRoll = Math.floor( Math.random() * 6 );
 
-document.querySelector("main").innerHTML = `<h1>You rolled a ${dieRoll}, refresh the browser to roll once again</h1>.` 
+document.querySelector("main").innerHTML = `<h1>You rolled a ${dieRoll}, refresh the browser to roll once again!</h1>.` 
 console.log(`You rolled a ${dieRoll}`);
